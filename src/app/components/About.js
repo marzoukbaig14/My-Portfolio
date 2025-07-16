23b14cfd142b6e20139bdcde77adc60d127cbb94
marzook baig <mbaig2@u.rochester.edu>
2025-07-15 21:12:21 -0400

    apos

--- src/app/components/About.js
+++ src/app/components/About.js
@@ -3,7 +3,7 @@ export default function About() {
     <section id="about" className="min-h-screen bg-gray-100 flex flex-col items-center justify-center px-4">
       <h2 className="text-4xl font-bold mb-6">About Me</h2>
       <p className="max-w-3xl text-lg leading-relaxed">
-        I'm a passionate software engineer with experience in Full Stack Development, Core software Development and Databases
+        I&apos;m a software developer. a passionate software engineer with experience in Full Stack Development, Core software Development and Databases
         I love React, Next.js, and creating clean user experiences.
       </p>
     </section>
